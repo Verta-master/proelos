1bcc6edd0f21fa25106b3d0f4676af25743f368e
Verta-master <vertyagina@gmail.com>
2020-11-18 10:23:38 +0600

    Update 2020-11-18T04:23:33.300Z

--- js/script.js
+++ js/script.js
@@ -30,3 +30,42 @@ $(".btn-top").click(function () {
   }, 400);
   return false;
 });
+
+//Scroll to menu anchor
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+
+  $('.menu__item a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('.menu').removeClass('menu--opened');
+    $('.overlay').removeClass('overlay--opened');
+    $(document).off("scroll");
+
+//    $('.menu__item a').each(function () {
+//      $(this).removeClass('menu__link--active');
+//    })
+//    $(this).addClass('menu__link--active');
+
+    var target = this.hash,
+      menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 500, 'swing', function () {
+      $(document).on("scroll", onScroll);
+    });
+  });
+});
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('.menu__link').each(function () {
+    event.preventDefault();
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('.menu__link').removeClass("menu__link--active");
+      currLink.addClass("menu__link--active");
+    }
+  });
+};
